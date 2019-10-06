211655962f468ad2377b71656352016286c6a05b
creativ073 <andrea.colanicchia@gmail.com>
2019-10-06 09:26:29 +0200

    feat(anagrafica): completata GET per ricevere lista dei file CSV caricati

--- pages/api/anagrafica/index.js
+++ pages/api/anagrafica/index.js
@@ -1,4 +1,5 @@
 import env from "../../../utils/env";
+import db from "../../../utils/db";
 const jwtAuth = require('micro-jwt-auth');
 const microCors = require('micro-cors');
 
@@ -7,7 +8,9 @@ const auth = jwtAuth(env.seed);
 
 export default auth(cors(async (req, res) => {
     if (req.method === 'GET') {
-        res.status(400).json({ error: "Nessun file" });
+        res.status(200).json({
+            "csv": db.get('csv').value()
+        });
     } else {
         res.status(400).json({ error: 'File non salvato' });
     }
